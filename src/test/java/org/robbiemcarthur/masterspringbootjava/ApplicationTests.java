2e4430a9028c2cd0a83e16e44d24486f417809c6
Robbie McArthur <robbiemca123@gmail.com>
2025-02-10 19:47:48 +0000

    Initial commit of SpringBoot app

---
+++ src/test/java/org/robbiemcarthur/masterspringbootjava/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.robbiemcarthur.masterspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
